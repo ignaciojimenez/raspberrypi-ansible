fd89aec43408a9c08454c7a456cc7f2e3d86e78a
Ignacio Jimenez <i.jimenezpi@gmail.com>
2023-03-27 23:05:16 +0200

    First custom host working playbooks

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # raspberrypi-ansible
+## Description
+This is a collection of scripts to kickstart raspberry pi's using a minimal raspbian installation and deploy configurations using ansible playbooks
+
+## Usage
+This repo has multiple tools. Use at suited.
+### Pre-bootup
+- `load_rpi`:   MacOS tool to load a base image to an SD card
+```
+Usage: load_rpi image_source hostname
+Parameters:
+ image_source   Where to install the image from. Options: { netinstall | downloadurl | raspbian }
+ hostname       Type of raspberry. Options: { pihole|cobra|hifipi|dockassist|devpi|vinylstreamer|pizero }
+```
+> Currently used `load_rpi raspbian [host]`
+
+### Post-bootup
+- `ansible-playbook installation.yml --limit=[host] -k --extra-vars "host=[host]"`: Ansible playbook to install a host. Note -k is only needed for the first run to add interactive initial password.
+Requires:
+- `python3 -m pip install --user ansible` - more info in [red hat docs](https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html). Useful to have:
+    - `python3 -m pip install --user argcomplete`
+- `ansible-galaxy collection install community.crypto`
+- `ansible-galaxy collection install ansible.posix`
+- passlib - `pip install passlib`
+- sshpass -- Deprecated, not needed anymore
+
+# Flavours so far
+## devpi
+Barebones install for testing
+
+## hifipi
+This raspberry pi will use a [Hifiberry](https://www.hifiberry.com/) hat to play high quality audio received via Airplay using [shairport](https://github.com/mikebrady/shairport-sync) or Spotify connect via [raspotify](https://github.com/dtcooper/raspotify). It also runs [mpd](https://www.musicpd.org) and is used to play streams controlled by remote clients such as `vinylstreamer`
+### TODO
+- [ ] Find out how to monitor and restart raspotify/airplay receivers if not working
+
+## Vinylstreamer
+This raspberry pi will run an [icecast](https://icecast.org) server that exposes an internet-radio stream, fed by a [liquidsoap](https://www.liquidsoap.info) defined audiostream using ogg/flac. It will also run a python script `detect_audio.py` that will detect an input audio stream and remotely control an mpd daemon to play the icecast stream
+### TODO
+- [ ] Define on installation the remote ip to control mpd in `detect_audio.py`
+
+## Cobra
+TBD
+
+## Pihole
+TBD
+
+## Dockassist
+TBD
+
---
+++ host_playbooks/vinylstreamer/detect_audio.py
@@ -0,0 +1,68 @@
+from array import array
+import pyaudio
+import subprocess
+import sys
+
+START_THRESHOLD = 150
+STOP_THRESHOLD = 50
+CHUNK_SIZE = 2048
+FORMAT = pyaudio.paInt16
+RATE = 48000
+# TODO pass these as args
+hifipi_ip="10.30.80.100"
+ls_stream="Turntable_stream"
+
+def is_silent(snd_data, threshold):
+    """
+    Returns 'True' if below the 'silent' threshold"
+    :param snd_data:
+    :return: True|False
+    """
+    return max(snd_data) < threshold
+
+
+def listen():
+    """
+    listen to default input device and perform an action if sound starts or stops
+    """
+    p = pyaudio.PyAudio()
+    snd_started = False
+    while 1:
+        # using default input -- needs to be dsnoop configured in alsa
+        stream = p.open(format=FORMAT, channels=2, rate=RATE, input=True, output=False, frames_per_buffer=CHUNK_SIZE)
+        # little endian, signed short
+        snd_data = array('h', stream.read(CHUNK_SIZE))
+        if sys.byteorder == 'big':
+            snd_data.byteswap()
+
+        #print(f"Current Noise data: {max(snd_data)}")
+        #time.sleep(0.5)
+
+        if (not snd_started) and (not is_silent(snd_data, START_THRESHOLD)):
+            sys.stdout.write(f"start stream. Volume: {max(snd_data)}\n")
+            start = subprocess.run(["mpc", f"--host={hifipi_ip}","play"], capture_output=True, text=True)
+            if start.stdout:
+                received_stream = start.stdout.splitlines()[0]
+                if received_stream != ls_stream:
+                    sys.stderr.write(f"Unexpected stream response. Received:{received_stream}, expected:{ls_stream}. Command:{start.args} mpc clear and add playlist again\n")
+                else:
+                    sys.stdout.write(f"mpc output:\n{start.stdout}")
+                    snd_started = True
+            elif start.stderr:
+                sys.stderr.write(f"mpc play error. Full command result:{start}. Ideally mpc clear and add here")
+                # TODO: do this
+                # mpc --host=${hifipi_ip} clear
+                # mpc --host=${hifipi_ip} add http://"${my_ip}":${def_icecast_port}/${ls_name}.ogg
+        elif snd_started and is_silent(snd_data, STOP_THRESHOLD):
+            sys.stdout.write(f"stop stream. Volume: {max(snd_data)}\n")
+            stop = subprocess.run(["mpc", f"--host={hifipi_ip}","stop"], capture_output=True, text=True)
+            if stop.stdout: sys.stdout.write(f"mpc output:\n{stop.stdout}")
+            if stop.stderr: sys.stderr.write(f"mpc error output:\n{stop.stderr}")
+            snd_started = False
+
+        stream.stop_stream()
+        stream.close()
+
+
+if __name__ == '__main__':
+    listen()
